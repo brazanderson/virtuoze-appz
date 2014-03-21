b0ba1db69986cbaa6cea3306ee035732f8ffaa21
andersonbraz <anderson.braz@brazoft.com.br>
2014-03-21 00:46:44 -0300

    gitignore

--- showcase/target/.generated/com/brazoft/virtuoze/showcase/i18n/Messages_.java
+++
@@ -1,8 +0,0 @@
-package com.brazoft.virtuoze.showcase.i18n;
-
-public class Messages_ implements com.brazoft.virtuoze.showcase.i18n.Messages {
-  
-  public java.lang.String headingText(java.lang.String arg0) {
-    return "H" + arg0 + ". Heading";
-  }
-}
